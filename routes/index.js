4da3ac5202c0a549f037afbfb2886acc054bd6e8
Robert Schade <roscha80@web.de>
2021-05-28 11:43:44 +0200

    Add express, routes, prettier, gitignore. Initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import * as notes from "./notes.js";
+
+const router = express.Router();
+
+router.get("/notes", notes.list);
+router.post("/notes", notes.create);
+router.get("/notes/:id", notes.read);
+router.post("/notes/:id", notes.update);
+router.delete("/notes/:id", notes.deleteNotes);
+
+export default router;
